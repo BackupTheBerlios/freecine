a2fdb4430ed06131ea4ae0873570b56c81624bd2
victorbcn <victorbcn>
2005-11-16 01:34:16 +0000

    *** empty log message ***

---
+++ CineOnline2.0/JavaSource/test/CineOnline.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class for Servlet: CineOnline
+ *
+ */
+ public class CineOnline extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public CineOnline() {
+		super();
+	}  
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 PrintWriter out = new PrintWriter(System.out);
+		 out = response.getWriter();
+		 out.println("Hello world!");
+		 out.close();
+	}  	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}   	
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+		    
+}
