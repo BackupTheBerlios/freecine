418fd8809726129ee296839ac2d83bebfb37b5e7
ivanmateu <ivanmateu>
2005-11-30 18:37:28 +0000

    Servlet gestioPelicules

--- cineonline2.1/JavaSource/servlets/GestioPeliculesServlet.java
+++ cineonline2.1/JavaSource/servlets/GestioPeliculesServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import gestioCinema.ControladorException;
 import gestioCinema.gestioPelicules.ControladorPelicules;
+import gestioCinema.gestioPelicules.Pelicula;
 
 import java.io.IOException;
 import java.util.Vector;
@@ -20,33 +21,48 @@ import javax.servlet.http.HttpServletResponse;
 	private static final long serialVersionUID = 1L;
 	private HttpServletRequest request;
 	private HttpServletResponse response;
-	private String urlExit="/jspIvan/pelicules.jsp";
-	private String urlError="/jspIvan/error.jsp";
+	private String urlExit="/intranet/pelicules.jsp";
+	private String urlError="/intranet/error.jsp";
 	private RequestDispatcher rd;
+	private ControladorPelicules ctrlPelicules;
 	
-
 	public GestioPeliculesServlet() {
 		super();
 	}   	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		this.request = request;
+		this.response = response;
+		rd = getServletContext().getRequestDispatcher(urlError+"?error=Accés denegat");
+	    rd.forward(request, response);
 	}  	
-	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.request = request;
 		this.response = response;
-	
-	}   	 
-	
-	private void llistarPeliculesAction() throws ServletException, IOException{
-		
-		ControladorPelicules ctrlPelicules;
+		String accio = request.getParameter("accio");
 		try {
+			
 			ctrlPelicules = new ControladorPelicules();
-			Vector catalegPelicules = ctrlPelicules.getPelicules();		
-			request.getSession().setAttribute("catalegPelicules", catalegPelicules);		
+			if(accio.equals("llistarPelicules")) llistarPeliculesAction();
+			else if(accio.equals("detallPelicula")) detallPeliculaAction();
+			else if(accio.equals("afegirPelicula")) afegirPeliculaAction();
+			else if(accio.equals("modificarPelicula")) modificarPeliculaAction();
+			else if(accio.equals("eliminarPelicula")) eliminarPeliculaAction();
+			
+			
+		} catch (ControladorException e) {
+			rd = getServletContext().getRequestDispatcher(urlError+"?Error=No es pot connectar a la base de dades");
+		}
+	}   	 
+
+	private void llistarPeliculesAction() throws ServletException, IOException{
+		urlExit="intranet/pelicules.jsp";
+		
+		try {
+			
+			Vector llistaPelicules = ctrlPelicules.getPelicules();		
+			request.getSession().setAttribute("llistaPelicules", llistaPelicules);		
 			
 		    rd = getServletContext().getRequestDispatcher(urlExit);
 		    rd.forward(request, response);
@@ -55,5 +71,31 @@ import javax.servlet.http.HttpServletResponse;
 		    RequestDispatcher rd = getServletContext().getRequestDispatcher(urlError+"?Error="+e.getMessage());
 		    rd.forward(request, response);
 		}
+	}
+	
+	private void detallPeliculaAction() throws ServletException, IOException{
+		urlExit="intranet/fitxa-pelicula.jsp";
+		int idPelicula = Integer.parseInt(request.getParameter("idPelicula"));
+		try {
+			
+			Pelicula pelicula = ctrlPelicules.getPelicula(idPelicula);		
+			request.getSession().setAttribute("pelicula", pelicula);		
+			
+		    rd = getServletContext().getRequestDispatcher(urlExit);
+		    rd.forward(request, response);
+		    
+		} catch (ControladorException e) {
+		    RequestDispatcher rd = getServletContext().getRequestDispatcher(urlError+"?Error="+e.getMessage());
+		    rd.forward(request, response);
+		}
+	}
+	
+	private void afegirPeliculaAction() throws ServletException, IOException{
+	}
+	
+	private void eliminarPeliculaAction() throws ServletException, IOException{
+	}
+
+	private void modificarPeliculaAction() throws ServletException, IOException{
 	}
 }
