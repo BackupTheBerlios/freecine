a0aaed441d35925d5613692fcf2e36d65b473844
ivanmateu <ivanmateu>
2005-12-09 11:48:37 +0000

    ok insertSales

--- cineonline2.1/WebContent/BBDD/codi/insertar_sales.sql
+++ cineonline2.1/WebContent/BBDD/codi/insertar_sales.sql
@@ -2,14 +2,3 @@ INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('1', 10, 25,
 INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('2', 15, 30, 'Tamany: 15x30');
 INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('3', 10, 25, 'Tamany: 10x25');
 INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('4', 20, 50, 'Tamany: 20x50');
-
-
-
-
-
-
---FET PER IVAN--
-INSERT INTO sala(id, nom, num_columnes, num_files, num_butaques, descripcio) VALUES (1,'Sala 1', 10, 25, 10*25,'Tamany: 10x25');
-INSERT INTO sala(id, nom, num_columnes, num_files, num_butaques, descripcio) VALUES (2,'Sala 2', 15, 30, 15*30,'Tamany: 15x30');
-INSERT INTO sala(id, nom, num_columnes, num_files, num_butaques, descripcio) VALUES (3,'Sala 3', 10, 25, 10*25,'Tamany: 10x25');
-INSERT INTO sala(id, nom, num_columnes, num_files, num_butaques, descripcio) VALUES (4,'Sala 4', 20, 50, 20*50,'Tamany: 20x50');
