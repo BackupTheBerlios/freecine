5ca4f8288c6e69553aef4c8bf0779c26c9dc5158
serginho <serginho>
2005-12-19 14:12:37 +0000

    Au que només resta comprar i reservar: nomes Intranet!

--- cineonline3.0/WebContent/BBDD/codiPrimer/BD_onlicine.sql
+++
@@ -1,3 +0,0 @@
-\i crear_taules.sql
-\i triggers.sql 
-\i insertar_dades.sql
--- cineonline3.0/WebContent/BBDD/codiPrimer/insertar_sales.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('1', 10, 25, 'Tamany: 10x25');
-INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('2', 15, 30, 'Tamany: 15x30');
-INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('3', 10, 25, 'Tamany: 10x25');
-INSERT INTO sala( nom, num_columnes, num_files, descripcio) VALUES ('4', 20, 50, 'Tamany: 20x50');
